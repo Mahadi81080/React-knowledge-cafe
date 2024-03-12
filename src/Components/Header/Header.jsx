c5c58a09734f7c3667088eef73fd504cc4d94439
Mahadi810803 <mahedihasan81080@gmail.com>
2024-03-13 00:37:28 +0600

    added  blog and customize section

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,10 @@
-
+import profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
-        <div className="text-4xl font-bold">
-            <h1>Knowledge Cafe</h1>
-        </div>
+        <header className='flex justify-between items-center p-4 mx-4 border-b-2'>
+            <h1 className="text-3xl font-bold">Knowledge Cafe</h1>
+            <img src={profile} alt="" />
+        </header>
     );
 };
 
